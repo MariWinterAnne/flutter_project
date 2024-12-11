26e513ef0b50cae2ec8ecec06539b0475c3153e0
Marianna <its-marianne@ya.ru>
2024-12-11 16:07:07 +0300

    Splash screen.

---
+++ lib/chat/modular/features/splash/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+setupSplash() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+    Timer(
+      const Duration(seconds: 2),
+      () {
+        Navigator.of(context).pushReplacementNamed('/home');
+      },
+    );
+  }
+
+  void initialization() async {
+    await Future.delayed(const Duration(seconds: 1));
+    FlutterNativeSplash.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: FlutterLogo(),
+      ),
+    );
+  }
+}
